10c2015f8f4f8dcc0c58e1c355e61889db9d38b5
nguyencuong23 <cng101078@gmail.com>
2026-01-28 21:23:37 +0700

    Theviet (#38)
    * viet
    
    * viet
    
    * viet
    
    * v
    
    * viet full
    
    * full
    
    ---------
    
    Co-authored-by: Dang Viet <raidene654@gmail.com>
    Co-authored-by: vietfronend <dangtheviet2410@gmail.com>

--- Migrations/20260128042708_AddShelfEntity.Designer.cs
+++ Migrations/20260128042708_AddShelfEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuanLyThuVienTruongHoc.Data;
 
@@ -11,9 +12,11 @@ using QuanLyThuVienTruongHoc.Data;
 namespace QuanLyThuVienTruongHoc.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260128042708_AddShelfEntity")]
+    partial class AddShelfEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -145,13 +148,11 @@ namespace QuanLyThuVienTruongHoc.Migrations
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ShelfId"));
 
                     b.Property<string>("Location")
-                        .HasMaxLength(200)
-                        .HasColumnType("nvarchar(200)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasMaxLength(100)
-                        .HasColumnType("nvarchar(100)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.HasKey("ShelfId");
 
---
+++ ViewModels/LibraryApiViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyThuVienTruongHoc.Models.ViewModels
+{
+    public class BorrowRequestViewModel
+    {
+        [Required]
+        public string BookId { get; set; }
+
+        [Required]
+        public string StudentId { get; set; }
+
+        [Required]
+        public DateTime ReturnDate { get; set; }
+    }
+
+    public class BookApiViewModel
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string image { get; set; }
+        public int available { get; set; }
+    }
+}
